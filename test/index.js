ac99558e0d4ce69d2c2cff6435b74c5d99c4cd4c
River Grimm <river.grimm@gmail.com>
2015-06-30 13:45:09 -0700

    handle incoming GET request

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ try {
 var exec = require('child_process').exec
 var fs = require('fs')
 var mkdirp = require('mkdirp')
+var request = require('request')
 var test = require('tape')
 var FormData = require('form-data')
 var memdown = require('memdown')
@@ -12,7 +13,9 @@ var township = require('township')
 var levelup = require('levelup')
 var db = levelup('db', {db: memdown})
 var app = require('../')
+var media = require('../lib/model')(db)
 
+var serverURI = 'http://127.0.0.1:4243/api/v1/media'
 var uploadDir = __dirname + '/blobs'
 var server
 
@@ -38,21 +41,24 @@ function after () {
   exec('rm -rf ' + uploadDir, function (err) {if (err) throw err})
 }
 
+/**
+ * Tests
+ */
 test('upload a file to disk storage', function (t) {
   t.plan(3)
   before({uploadDir: uploadDir})
   var form = new FormData()
   var response = ''
   form.append('foo', fs.createReadStream(__dirname + '/fixtures/test.png'))
-  form.submit('http://127.0.0.1:4243/api/v1/media', function (err, res) {
+  form.submit(serverURI, function (err, res) {
     t.equal(err, null)
-    t.ok(res, 'should be ok')
+    t.ok(res)
     res.on('data', function (chunk) {
       response += chunk
     })
     res.on('end', function () {
-      var filename = JSON.parse(response)[0]
-      t.equal(filename, 'test.png')
+      var file = JSON.parse(response)[0]
+      t.equal(file.name, 'test.png')
       after()
     })
   })
@@ -62,22 +68,41 @@ test('upload multiple files to disk storage', function (t) {
   t.plan(4)
   before({uploadDir: uploadDir})
   var form = new FormData()
-  var expected = ['test.png', 'test.png']
+  var expected = [{name: 'test.png'}, {name: 'test.png'}]
   var response = ''
   form.append('foo', fs.createReadStream(__dirname + '/fixtures/test.png'))
   form.append('bar', fs.createReadStream(__dirname + '/fixtures/test.png'))
-  form.submit('http://127.0.0.1:4243/api/v1/media', function (err, res) {
+  form.submit(serverURI, function (err, res) {
     t.equal(err, null)
-    t.ok(res, 'should be ok')
+    t.ok(res)
     res.on('data', function (chunk) {
       response += chunk
     })
     res.on('end', function () {
       var files = JSON.parse(response)
       for (var i = files.length; i--;) {
-        t.equal(files[i], expected[i])
+        t.equal(files[i].name, expected[i].name)
       }
 
+      after()
+    })
+  })
+})
+
+test('get a list of available media', function (t) {
+  t.plan(5)
+  before({uploadDir: uploadDir})
+  var form = new FormData()
+
+  form.append('foo', fs.createReadStream(__dirname + '/fixtures/test.png'))
+  form.submit(serverURI, function (err, res) {
+    t.equal(err, null)
+    t.ok(res)
+    request(serverURI, function (error, response, body) {
+      t.equal(error, null)
+      t.ok(response)
+      body = JSON.parse(body)
+      t.equal(body[0].value.name, 'test.png')
       after()
     })
   })
@@ -93,16 +118,29 @@ test('upload a file to s3', function (t) {
   var form = new FormData()
   var response = ''
   form.append('foo', fs.createReadStream(__dirname + '/fixtures/test.png'))
-  form.submit('http://127.0.0.1:4243/api/v1/media', function (err, res) {
+  form.submit(serverURI, function (err, res) {
     t.equal(err, null)
-    t.ok(res, 'should be ok')
+    t.ok(res)
     res.on('data', function (chunk) {
       response += chunk
     })
     res.on('end', function () {
-      var filename = JSON.parse(response)[0]
-      t.equal(filename, 'test.png')
+      var file = JSON.parse(response)[0]
+      t.equal(file.name, 'test.png')
       after()
     })
   })
+})
+
+test('teardown media', function (t) {
+  media.createReadStream()
+    .on('data', function (data) {
+      media.delete(data.key, function (err) {
+        t.notOk(err)
+      })
+    })
+    .on('end', function () {
+      db.close()
+      t.end()
+    })
 })
