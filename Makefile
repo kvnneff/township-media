f33400e6126224b8b6b9db7ffa5d46bd0f795285
River Grimm <river.grimm@gmail.com>
2015-07-01 11:26:22 -0700

    add all route handlers

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
 BIN := ./node_modules/.bin
+TAPE := $(BIN)/tape
+TAP-SPEC := ./node_modules/tap-spec/bin/cmd.js
 
 lint: node_modules
 	@$(BIN)/standard *.js test/**/*.js
 
 test: node_modules lint
-	@$(BIN)/tape test/**/*.js | tap-spec
+	@$(TAPE) test/**/*.js | $(TAP-SPEC)
 
 node_modules:
 	npm install
--- test/index.js
+++ test/index.js
@@ -16,7 +16,7 @@ var app = require('../')
 var media = require('../lib/model')(db)
 
 var serverURI = 'http://127.0.0.1:4243/api/v1/media'
-var uploadDir = __dirname + '/blobs'
+var uploadDir = __dirname + '/media_uploads'
 var server
 
 /**
@@ -24,9 +24,7 @@ var server
  */
 function before (pluginOptions) {
   pluginOptions = pluginOptions || {}
-  server = township(db, {
-    apps: [app(db, pluginOptions)]
-  })
+  server = township(db, {apps: [app(db, pluginOptions)]})
 
   server.listen()
 
@@ -89,7 +87,7 @@ test('upload multiple files to disk storage', function (t) {
   })
 })
 
-test('get a list of available media', function (t) {
+test('get a list of media resources', function (t) {
   t.plan(5)
   before({uploadDir: uploadDir})
   var form = new FormData()
@@ -104,6 +102,29 @@ test('get a list of available media', function (t) {
       body = JSON.parse(body)
       t.equal(body[0].value.name, 'test.png')
       after()
+    })
+  })
+})
+
+test('get a single media resource', function (t) {
+  before({uploadDir: uploadDir})
+  var form = new FormData()
+  var response = ''
+  form.append('foo', fs.createReadStream(__dirname + '/fixtures/test.png'))
+  form.submit(serverURI, function (err, res) {
+    if (err) throw err
+    res.on('data', function (chunk) {
+      response += chunk
+    })
+    res.on('end', function () {
+      var file = JSON.parse(response)[0]
+      request(serverURI + '/' + file.key, function (error, response, body) {
+        t.equal(error, null, 'error is null')
+        t.ok(response, 'response is truthy')
+        t.equal(JSON.parse(body).name, 'test.png')
+        after()
+        t.end()
+      })
     })
   })
 })
@@ -132,11 +153,61 @@ test('upload a file to s3', function (t) {
   })
 })
 
+test('delete a file from disk storage', function (t) {
+  before({uploadDir: uploadDir})
+  var form = new FormData()
+  var response = ''
+  form.append('foo', fs.createReadStream(__dirname + '/fixtures/test.png'))
+  form.submit(serverURI, function (err, res) {
+    if (err) throw err
+    res.on('data', function (chunk) {
+      response += chunk
+    })
+    res.on('end', function () {
+      var file = JSON.parse(response)[0]
+      request.del(serverURI + '/' + file.key, function (error, response, body) {
+        t.equal(error, null, 'error is null')
+        t.ok(response, 'response is truthy')
+        t.equal(response.statusCode, 204, 'response.statusCode is 204')
+        after()
+        t.end()
+      })
+    })
+  })
+})
+
+test('delete a file from s3', function (t) {
+  before({
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET_KEY,
+    bucket: process.env.S3_BUCKET
+  })
+  var form = new FormData()
+  var response = ''
+  form.append('foo', fs.createReadStream(__dirname + '/fixtures/test.png'))
+  form.submit(serverURI, function (err, res) {
+    if (err) throw err
+    res.on('data', function (chunk) {
+      response += chunk
+    })
+    res.on('end', function () {
+      var file = JSON.parse(response)[0]
+      request.del(serverURI + '/' + file.key, function (error, response, body) {
+        t.equal(error, null, 'error is null')
+        t.ok(response, 'response is truthy')
+        t.equal(response.statusCode, 204, 'response.statusCode is 204')
+        after()
+        t.end()
+      })
+    })
+  })
+})
+
 test('teardown media', function (t) {
   media.createReadStream()
     .on('data', function (data) {
       media.delete(data.key, function (err) {
-        t.notOk(err)
+        t.notOk(err, 'no error deleting model')
       })
     })
     .on('end', function () {
